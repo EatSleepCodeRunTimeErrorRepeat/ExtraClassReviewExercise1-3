9f5534f7fd5158af01ba3f90a2866c9be5039438
EatSleepRunTimeErrorRepeat <ryan.letchman@mail.kmutt.ac.th>
2023-02-20 23:33:09 +0700

    EX3 1st try

--- js3.js
+++ js3.js
@@ -3,6 +3,17 @@
 //It is responsible for setting and clearing the fields in Billing Information 
 //get references to the input elements
 
+function billingFunction(){
+    let check = document.getElementById("same")
+    // document.querySelectorAll("#same")
+    if(check.checked){
+        let Sname = document.getElementById("shippingName").value
+        let Szip = document.getElementById("shippingZip").value
+        document.getElementById("billingName").value = `${Sname}`
+        document.getElementById("billingZip").value = `${Szip}`
+    }
+}
+
 
 const shippingNameInput = document.getElementById("shipping-name");
 const shippingZipInput = document.getElementById("shipping-zip");
