a89d331817310c7de625cdb0cf65a0c4fa4e0a2e
EatSleepRunTimeErrorRepeat <ryan.letchman@mail.kmutt.ac.th>
2023-02-20 23:26:06 +0700

    Starting on EX3

--- js.js
+++ js.js
@@ -14,7 +14,7 @@ function checkFirstChar(){
         console.log("String's first character is not uppercase");
     }
 }
-*/
+
 
 var fruits = ["Apple", "Mango", "Orange", "Grape"];
 
@@ -38,5 +38,30 @@ function myInput(){
     }
 }
 
+
+*/
+
+
 /*Add the JavaScript here for the function billingFunction().
 It is responsible for setting and clearing the fields in Billing Information */
+// get references to the input elements
+
+
+const shippingNameInput = document.getElementById("shipping-name");
+const shippingZipInput = document.getElementById("shipping-zip");
+const billingNameInput = document.getElementById("billing-name");
+const billingZipInput = document.getElementById("billing-zip");
+const sameAsShippingCheckbox = document.getElementById("same-as-shipping");
+
+// add event listener to the checkbox
+sameAsShippingCheckbox.addEventListener("change", function() {
+  if (this.checked) {
+    // copy values from shipping to billing
+    billingNameInput.value = shippingNameInput.value;
+    billingZipInput.value = shippingZipInput.value;
+  } else {
+    // clear values in billing
+    billingNameInput.value = "";
+    billingZipInput.value = "";
+  }
+});
